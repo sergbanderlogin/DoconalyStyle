08e19b1c3e7d195c3608e0e08253fdfdf91f1eb6
sergbanderlogin <sergbanderlogin@gamil.com>
2020-10-25 20:57:58 +0200

    merge ProductScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,13 @@ import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from "./screens/ProductScreen.js";
 import {parseRequestUrl} from "./utils.js";
 import Error404Screen from "./screens/Error404Screen.js";
+import CartScreen from "./screens/CartScreen";
+
 const routes = {
     "/": HomeScreen,
     "/product/:id": ProductScreen,
+    "/cart/:id": CartScreen,
+    "/cart": CartScreen,
 }
 const router = async () => {
     const request = parseRequestUrl();
@@ -14,6 +18,7 @@ const router = async () => {
     const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
     const main = document.getElementById("main-container");
     main.innerHTML = await screen.render();
+    await screen.after_render();
 };
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
