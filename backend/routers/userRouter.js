ac60e7a41161f43f2cdde12c1d06463e6cc62e31
sergbanderlogin <sergbanderlogin@gamil.com>
2020-10-30 02:16:52 +0200

    create block Sign-In

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import expressAsyncHandler from "express-async-handler";
 import User from "../models/userModel";
+import {generateToken} from "../utils";
 
 const userRouter = express.Router();
 
-userRouter.get("/createadmin", async (req, res) => {
+userRouter.get("/createadmin", expressAsyncHandler(async (req, res) => {
 	try {
 		const user = new User({
 			name: 'admin',
@@ -14,8 +16,28 @@ userRouter.get("/createadmin", async (req, res) => {
 		const createUser = await user.save();
 		res.send(createUser);
 	} catch(err){
-		res.status(5000).send({message: err.message});
+		res.status(500).send({message: err.message});
 	}
-});
+}));
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+	const signUser = await User.findOne({
+		email: req.body.email,
+		password: req.body.password,
+	});
+	if (!signinUser){
+		res.status(401).send({
+			message: 'Invalid Email or Password',
+		});
+	} else {
+		res.send({
+			_id: signinUser._id,
+			name: signinUser.name,
+			email: signinUser.email,
+			isAdmin: signinUser.isAdmin,
+			token: generateToken(signinUser),
+		})
+	}
+})
+);
 
 export default userRouter;
